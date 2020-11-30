76d4bfc87a88891d12467a288af75920ba780b32
DESKTOP-AS8VSPR\sia <2672836947@qq.com>
2020-11-30 11:45:26 +0800

    人力资源

--- 人力资源/人力资源/Form1.cs
+++ 人力资源/人力资源/Form1.cs
@@ -8,18 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace OA
+namespace 人力资源
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            一度方法不负有客服
         }
     }
 }
--- 人力资源/人力资源/Form2.cs
+++ 人力资源/人力资源/Form2.cs
@@ -8,18 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace OA
+namespace 人力资源
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            一度方法不负有客服
         }
     }
 }
